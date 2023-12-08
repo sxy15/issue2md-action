9c73eed7245a98735ba63b4b2b50541e2a9ab62e
sxy15 <131671708+sxy15@users.noreply.github.com>
2023-12-08 11:21:48 +0000

    feat: dev

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
-import { githubToken, getIssues } from './utils'
+import { getIssues } from './utils'
 
 async function run() {
-    const token = githubToken()
 
-    console.log('token', token)
     const issues = await getIssues()
 
-    console.log('issues', issues)
+    const tableData = issues.map(issue => {
+        const { html_url, title, updated_at } = issue
+        return {
+            html_url,
+            title,
+            updated_at
+        }
+    })
+
+    const table = tableData.map(item => {
+        return `| [${item.title}](${item.html_url}) | ${item.updated_at} |`
+    }).join('\n')
+
+    const md = `# Issues
+                | title | updated_at |
+                | --- | --- |
+                ${table}
+                `
+    console.log(md)
 }
 
 
